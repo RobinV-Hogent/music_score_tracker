650ebdd1073d464e1749e02795523eb1ee159b92
Robin Vermeir <robin.v.vermeir@gmail.com>
2025-08-16 22:34:14 +0200

    frontend: fixing layouts + score_cards

--- music_score_tracker/music_scores/views.py
+++ music_score_tracker/music_scores/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def welcome(request):
-    return render(request, 'pages/welcome.html')
+    return render(request, 'pages/welcome.html', {'scores': list(range(10))})
