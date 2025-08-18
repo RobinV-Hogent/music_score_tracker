3cafdef2c4dd693385431a38ee925256feaacfcf
Robin Vermeir <robin.v.vermeir@gmail.com>
2025-08-18 16:16:52 +0200

    frontend: score form uppercase enum fix

--- music_score_tracker/music_scores/models/score.py
+++ music_score_tracker/music_scores/models/score.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from music_scores.enums import ScoreSignatures, ScoreDifficulty, ScoreInstrument
 from enum import Enum
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -9,11 +10,11 @@ from enum import Enum
 class Score(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    signature = models.CharField(max_length=50, choices=[(tag.value, tag.name) for tag in ScoreSignatures], default=ScoreSignatures.FOURFOUR.value)
-    difficulty = models.CharField(max_length=100, choices=[(tag.value, tag.name) for tag in ScoreDifficulty], default=ScoreDifficulty.INTERMEDIATE.value)
-    instrument = models.CharField(max_length=100, choices=[(tag.value, tag.name) for tag in ScoreInstrument], default=ScoreInstrument.PIANO.value)
-    measure_count = models.IntegerField(default=10, max_length=500)
-    beats_per_minute = models.IntegerField(default=1,  max_length=250)
+    signature = models.CharField(max_length=50, choices=[(tag.name, tag.value) for tag in ScoreSignatures], default=ScoreSignatures.FOURFOUR.value)
+    difficulty = models.CharField(max_length=100, choices=[(tag.name, tag.value) for tag in ScoreDifficulty], default=ScoreDifficulty.INTERMEDIATE.value)
+    instrument = models.CharField(max_length=100, choices=[(tag.name, tag.value) for tag in ScoreInstrument], default=ScoreInstrument.PIANO.value)
+    measure_count = models.IntegerField(default=10,  validators=[MinValueValidator(1), MaxValueValidator(500)])
+    beats_per_minute = models.IntegerField(default=100, validators=[MinValueValidator(1), MaxValueValidator(250)])
     
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
